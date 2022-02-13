42da2a44fcae20d11064175f0600fdf138f211b3
RotaMedicsApp <yinkyade@gmail.com>
2022-02-13 18:42:26 +0100

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/the_matrix_resurrection/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.the_matrix_resurrection
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'matrix_effect.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: [SystemUiOverlay.bottom]);
+    return const Scaffold(
+        backgroundColor: Colors.black,
+        body: MatrixEffect()
+    );
+  }
+}
